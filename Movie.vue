09ca779719b720f2c01ee3e3b5f75586c5cdb1ad
99sgabi <99sgabi@wp.pl>
2021-05-15 13:05:26 +0200

    server local host + cos nie dizala z vue

---
+++ Movie.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h1>
+        Movie name: {{ movie.name }}
+    </h1>
+    <p>
+        Date: {{ movie.dateOfRelease }}
+    </p>
+    <p>
+        Genere: {{ movie.genere }}
+    </p>
+    <ul>
+        <li v-for="role in cast" v-bind:key="role.actorId">
+           {{ role.actor && role.actor.name }}
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+    export default{
+        props: ["movie", "cast"],
+    };
+</script>
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
--- javascript.js
+++ javascript.js
@@ -1,23 +1,53 @@
+import Vue from "vue";
+import Movie from "./Movie.vue";
+
+
 var mainElement = new Vue({
     el: "#app",
     data: { 
         movieDatabase: {},
+        movieActorsDatabase: [],
         actorsDatabase: {},
+    },
+    methods: {
+        getActors(movieId)
+        {
+            let filteredElements = this.movieActorsDatabase.filter( function(movieActor){
+                return movieActor.filmId == movieId
+            })
+            return filteredElements
+        },
+    },
+    computed: {
+        filteredMovies(nameCriteria, dateCriteria, actorsCriteria, genereCriteria)
+        {
+
+        }
+    },
+    components:{
+        Movie,
     }
 })
-var baseUrl = "http://localhost:3000";
-var moviesBaseUrl = "http://localhost:3000/films";
-var actorsBaseUrl = "http://localhost:3000/actors";
-var castBaseUrl = "http://localhost:3000/cast";
+var baseUrl = "http://localhost:3001";
+var moviesBaseUrl = "http://localhost:3001/films";
+var actorsBaseUrl = "http://localhost:3001/actors";
+var movieActorsURL = (id) => `http://localhost:3001/films/${id}/actors`;
+var moviesActorsAllURL = "http://localhost:3001/films/actors";
+var actorsMovieURL =  (id) => `http://localhost:3001/actors/${id}/films`;
 
-//loading a list of movies
+//loading a list of movies with actors
 axios.get(moviesBaseUrl)
-    .then(response => {
-        mainElement.movieDatabase = response.data
+    .then(moviesResponse =>{
+        mainElement.movieDatabase = moviesResponse.data;
     })
     .catch(error => console.log(error))     
+
+axios.get(moviesActorsAllURL)
+    .then(response =>{
+        mainElement.movieActorsDatabase = response.data;
+    })
+    .catch(error => console.log(error))   
 
 axios.get(actorsBaseUrl)
     .then(response => {mainElement.actorsDatabase = response.data})
     .catch(error => console.log(error))
-
